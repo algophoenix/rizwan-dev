23acad884a868a5fcb77a5b5b738463b08150b86
algophoenix <algophoenix@users.noreply.github.com>
2026-03-16 07:56:09 +0000

    [MR] - P004 | Performance improvement

--- src/components/ui/MagneticButton.tsx
+++ src/components/ui/MagneticButton.tsx
@@ -39,3 +39,4 @@ export const MagneticButton = ({ children, className, ...props }: MagneticButton
 // TODO: future optimization 2026-02-18
 // TODO: future optimization 2026-03-05
 // TODO: future optimization 2026-03-09
+// TODO: future optimization 2026-03-16
