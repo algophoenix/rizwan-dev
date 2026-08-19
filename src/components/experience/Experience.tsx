e25252686e33b6db134d7fc4398ada02de057c2e
algophoenix <algophoenix@users.noreply.github.com>
2026-08-19 05:29:02 +0000

    [MR] - P008 | Styling improvement

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -90,3 +90,4 @@ export const Experience = () => {
 // TODO: future optimization 2026-07-01
 // TODO: future optimization 2026-07-07
 // TODO: future optimization 2026-07-15
+// TODO: future optimization 2026-08-19
